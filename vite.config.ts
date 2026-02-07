1ea39680a1b4ef23a32ede6396a7d5dbd6de53c4
Your Name <your.email@example.com>
2026-02-07 01:19:35 +0100

    Fix Vercel build failure by increasing PWA cache size limit

--- vite.config.ts
+++ vite.config.ts
@@ -35,6 +35,9 @@ export default defineConfig(({ mode }) => {
               type: 'image/png'
             }
           ]
+        },
+        workbox: {
+          maximumFileSizeToCacheInBytes: 4000000
         }
       })
     ],
